f5070cb293e80262d60f23cf8bc83671a09ab805
Samjacob007 <samjacob8545.sj@gmail.com>
2023-11-07 00:38:12 +0000

    added data

--- visualizaation.py
+++ visualizaation.py
@@ -1,3 +1,15 @@
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt 
+import numpy as np
+import matplotlib.pyplot as plt
+df=pd.read_csv("Dataset/NetflixUserbase.csv")
+
+male_users = df[df['Gender']=="Male"]
+male_users_count = male_users.groupby("Join Date").size()
+
+female_users = df[df['Gender']=="Female"]
+female_users_count = female_users.groupby("Join Date").size()
+
+
+plt.plot(female_users_count)
+plt.plot(male_users_count)
+plt.show()
